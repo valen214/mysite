5e86322c3da74287dff2a6850ac17b1ed1d7721b
valen214 <xvalen214x@gmail.com>
2020-09-16 18:09:01 +0800

    update

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ console.log('\n'.repeat(5) + "running server.ts");
 
 
 import path from "path";
+import fs from "fs";
 
 import express from "express";
 
@@ -20,6 +21,7 @@ app.use(middleware(compiler, {
   noInfo: true
 }));
 
+
 */
 
 console.log("__dirname:", __dirname);
@@ -28,8 +30,12 @@ app.use(express.static(path.join(__dirname, "../dist")));
 
 app.use(sync_session);
 
-app.get(/.*/, (req, res, next) => {
-  console.log(req.method, req.path, "HTTP/" + req.httpVersion);
+app.all(/.*/, (req, res, next) => {
+  let req_line = `${req.method} ${req.path} HTTP/${req.httpVersion}`;
+  let out = Array.from(req_line).map((v, i) => ( `\x1B\x5B38;5;${
+    Math.floor((232 - 17) * Math.random()) + 17
+  }m` + v )).join("");
+  console.log(out);
   next();
 });
 
@@ -47,7 +53,28 @@ app.get('/test', (req, res, next) => {
 app.get("/test", (req, res) => {
   res.write("OAKAKSKSK");
   res.end();
-})
+});
+
+
+app.get("/multi", (req, res) => {
+  res.type('text/html; charset=utf-8');
+  res.sendFile(path.join(__dirname, '../server/pages', 'multi.html'));
+});
+
+const multer = require("multer");
+const upload = multer();
+app.post("/multi", upload.single("f"), (req, res) => {
+  console.log("req.headers:", req.headers);
+  console.log(Object.keys(req));
+  // @ts-ignore
+  // console.log(req.file, req.client);
+  req.client.on("data", (chunk) => {
+    console.log("chunk:", chunk);
+  });
+  // @ts-ignore
+  fs.writeFile(path.join(__dirname, "temp", req.file.originalname || "img.webp"), req.file.buffer, null, () => {});
+  res.send(JSON.stringify(req.body));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
@@ -64,7 +91,7 @@ https://www.coreycleary.me/separating-logic-from-express-routes-for-easier-testi
 
 
 
-
+http://ascii-table.com/ansi-escape-sequences.php
 
 
 
